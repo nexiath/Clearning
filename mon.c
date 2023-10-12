c00ae607d60dcde6009397c448f9d018bc9c50d5
Robin Cassard <robin.cassard@edu.univ-fcomte.fr>
2023-10-12 22:40:20 +0200

    on continue trql

---
+++ mon.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int tab[] = {1, 2, 6, 3, 9, 4};
+    int taille = sizeof(tab) / sizeof(tab[0]);
+    int max = tab[0];
+    for (int i = 1; i < taille; i++)
+    {
+        if (tab[i] > max)
+        {
+            max = tab[i];
+        }
+    }
+    printf("l'élément le plus grand est : %d\n", max);
+    return 0;
+}
