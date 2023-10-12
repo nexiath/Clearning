a3a2d32ee5b7b87a5f61bbf4841f57b250d7c4a8
Robin Cassard <robin.cassard@edu.univ-fcomte.fr>
2023-10-12 22:29:17 +0200

    on continue trql

---
+++ tim.c
@@ -0,0 +1,10 @@
+int main(int argc, char **argv){
+
+    float r, a, p;
+    printf("entrez un radius\n");
+    scanf("%f", &r);
+    a = (r*r*M_PI);
+    printf("L'aire est de : %f\n", a);
+    p = (2*r*M_PI);
+    printf("Le périmetre est de %f\n", p);
+}
