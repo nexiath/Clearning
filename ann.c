7f0a32159137469bc294bf396466bff7e876b982
Robin Cassard <robin.cassard@edu.univ-fcomte.fr>
2023-10-12 22:19:17 +0200

    on continue trql

---
+++ ann.c
@@ -0,0 +1,12 @@
+int anniversaire(char name[], int age) {
+     printf("Joyeux anniversaire %s\n", name);
+     printf("tu es agé de %d ans\n", age);
+}
+
+int main(){
+    char name[] = "matheo";
+    int age = 15;
+
+    anniversaire(name, age);
+    return 0;
+}
