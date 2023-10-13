764a110de2722cc6cb634fc78f8e1546a401be19
nexiath <108474515+nexiath@users.noreply.github.com>
2023-10-13 12:33:15 +0200

    Update ann.c

--- ann.c
+++ ann.c
@@ -2,7 +2,6 @@ int anniversaire(char name[], int age) {
      printf("Joyeux anniversaire %s\n", name);
      printf("tu es agé de %d ans\n", age);
 }
-
 int main(){
     char name[] = "matheo";
     int age = 15;
