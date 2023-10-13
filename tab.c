8e0f7ac8e0a06631e4df83a9cc191b7efc90a6d5
nexiath <108474515+nexiath@users.noreply.github.com>
2023-10-13 12:29:45 +0200

    Update tab.c

--- tab.c
+++ tab.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(){
     FILE *fw;
     fw = fopen("text.txt", "w");
@@ -8,17 +7,14 @@ int main(){
         perror("erreur lors de l'écriture");
         return 1;
     }
-
     int tab[60];
     int n = sizeof(tab) / sizeof(tab[0]);
     tab[0] = 1;
-
     for(int i = 0; i<n; i++){
         tab[i] = i+1;
         // printf("%d\n", tab[i]);
         fprintf(fw, "%d\n", tab[i]);
     }
-
     fclose(fw);
     return 0;
 }
